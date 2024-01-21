265ffc8bab751223290b006c60ae1b2cfbc6081c
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 02:13:14 -0500

    multi handler update

--- src/main/java/com/insurgencedev/ecoquestsaddon/EcoQuestsAddon.java
+++ src/main/java/com/insurgencedev/ecoquestsaddon/EcoQuestsAddon.java
@@ -1,14 +1,17 @@
 package com.insurgencedev.ecoquestsaddon;
 
 import com.insurgencedev.ecoquestsaddon.listeners.EcoQuestsEventListener;
+import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
-@IBoostersAddon(name = "EcoQuestsAddon", version = "1.0.0", author = "InsurgenceDev", description = "EcoQuests Support")
+@IBoostersAddon(name = "EcoQuestsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoQuests Support")
 public class EcoQuestsAddon extends InsurgenceBoostersAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        registerEvent(new EcoQuestsEventListener());
+        if (Bukkit.getPluginManager().isPluginEnabled("EcoQuests")) {
+            registerEvent(new EcoQuestsEventListener());
+        }
     }
 }
--- src/main/java/com/insurgencedev/ecoquestsaddon/listeners/EcoQuestsEventListener.java
+++ src/main/java/com/insurgencedev/ecoquestsaddon/listeners/EcoQuestsEventListener.java
@@ -12,23 +12,23 @@ public final class EcoQuestsEventListener implements Listener {
     @EventHandler
     private void onGain(PlayerTaskExpGainEvent event) {
         final String TYPE = "Quests";
-        final String NAMESPACE = "ECO_MISC";
+        final String NAMESPACE = "ECO_QUESTS";
         double totalMulti = 1;
 
         IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getPlayer()).findActiveBooster(TYPE, NAMESPACE);
         if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            totalMulti += getMulti(boosterResult.getBooster().getMultiplier());
+            totalMulti += boosterResult.getBooster().getMultiplier();
         }
 
         GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(TYPE, NAMESPACE);
         if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            totalMulti += getMulti(boosterResult.getBooster().getMultiplier());
+            totalMulti += boosterResult.getBooster().getMultiplier();
         }
 
-        event.setAmount(event.getAmount() * totalMulti);
+        event.setAmount(calculateAmount(event.getAmount(), totalMulti));
     }
 
-    private double getMulti(double amount) {
-        return (amount >= 1) ? amount - 1 : amount;
+    private long calculateAmount(double amount, double multi) {
+        return (long) (amount * (multi < 1 ? 1 + multi : multi));
     }
 }
