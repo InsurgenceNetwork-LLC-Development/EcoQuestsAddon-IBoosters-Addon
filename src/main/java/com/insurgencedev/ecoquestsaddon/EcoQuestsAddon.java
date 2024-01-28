84785d389d3d1637f7dde29dae790bdd723a891d
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 19:07:19 -0500

    api jar update

--- src/main/java/com/insurgencedev/ecoquestsaddon/EcoQuestsAddon.java
+++ src/main/java/com/insurgencedev/ecoquestsaddon/EcoQuestsAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.ecoquestsaddon;
 
 import com.insurgencedev.ecoquestsaddon.listeners.EcoQuestsEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EcoQuestsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoQuests Support")
 public class EcoQuestsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("EcoQuests")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("EcoQuests")) {
             registerEvent(new EcoQuestsEventListener());
         }
     }
